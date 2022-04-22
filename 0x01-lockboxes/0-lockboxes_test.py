2a3e92008bf99b5418a3c184e6f5c1796c7a40bc
Sebatian <sebastian@nufi.mx>
2022-04-21 23:00:44 -0500

    Fix formating

--- 0x01-lockboxes/0-lockboxes_test.py
+++ 0x01-lockboxes/0-lockboxes_test.py
@@ -13,5 +13,5 @@ canUnlockAll = __import__('0-lockboxes').canUnlockAll
 # boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 # print(canUnlockAll(boxes))
 
-boxes = [[],[]]
+boxes = []
 print(canUnlockAll(boxes))
