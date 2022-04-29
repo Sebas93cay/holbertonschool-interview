8e3079a547ead4bef2e917fe1f83419f40d96702
Sebatian <sebastian@nufi.mx>
2022-04-28 23:35:31 -0500

    Fix pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-In a text file, there is a single character H. Your text editor can execute only
-two operations in this file: Copy All and Paste. Given a number n, write a method
-that calculates the fewest number of operations needed to result in exactly n H
-characters in the file.
+In a text file, there is a single character H. Your text editor
+can execute only two operations in this file: Copy All and Paste.
+Given a number n, write a method that calculates the fewest number
+of operations needed to result in exactly n H characters in the file.
 """
 
 
 def minOperations(n):
     """
-    In a text file, there is a single character H. Your text editor can execute only
-    two operations in this file: Copy All and Paste. Given a number n, write a method
-    that calculates the fewest number of operations needed to result in exactly n H
-    characters in the file.
+    In a text file, there is a single character H. Your text editor
+    can execute only two operations in this file: Copy All and Paste.
+    Given a number n, write a method that calculates the fewest number
+    of operations needed to result in exactly n H characters in the file.
     """
     if n <= 1:
         return 0
