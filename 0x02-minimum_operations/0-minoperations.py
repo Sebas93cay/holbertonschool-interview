e629831a1c41b031fd730f9427ce4d0fc2ae5913
Sebatian <sebastian@nufi.mx>
2022-04-28 23:31:58 -0500

    min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single character H. Your text editor can execute only
+two operations in this file: Copy All and Paste. Given a number n, write a method
+that calculates the fewest number of operations needed to result in exactly n H
+characters in the file.
+"""
+
+
+def minOperation(n):
+    """
+    In a text file, there is a single character H. Your text editor can execute only
+    two operations in this file: Copy All and Paste. Given a number n, write a method
+    that calculates the fewest number of operations needed to result in exactly n H
+    characters in the file.
+    """
+    if n <= 1:
+        return 0
+    return sum(getFactors(n))
+
+
+def getFactors(n):
+    """
+    Return a list with the prime factor of a number
+    """
+    i = 2
+    factors = []
+    while n > 1:
+        if n % i == 0:
+            factors.append(i)
+            n = n / i
+        else:
+            i = i + 1
+    return factors
---
+++ 0x02-minimum_operations/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+getFactors = __import__('0-minoperations').getFactors
+minOperation = __import__('0-minoperations').minOperation
+
+
+# print(getFactors(234))
+print(minOperation(9))
+print(minOperation(100))
